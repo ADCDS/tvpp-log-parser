f81c5adeeb4c3ee91a6bb770b9e76661341d450a
Turox <adriel@adriel.eu>
2019-08-17 16:48:06 -0300

    [BUG Fix] Nodes that weren't connected to source node caused StarTopology to generate vertices with 'Infinity' coords

--- src/parserLib/Graph/Visualizer/Topology/StarTopology.js
+++ src/parserLib/Graph/Visualizer/Topology/StarTopology.js
@@ -10,7 +10,7 @@ class StarTopology extends Topology {
 		this.source = options.source;
 
 		if (this.distancesFromSource == null || this.fathers == null) {
-			if(this.source == null){
+			if (this.source == null) {
 				throw "Invoked StarTopology without dijkstra data and source";
 			}
 
@@ -33,12 +33,29 @@ class StarTopology extends Topology {
 		//Distances preprocessor
 		let numberOfNodesAtRing = {0: 1};
 		let iterNumRing = {0: 0};
+
+		/**
+		 * Lets pull all infinity nodes to the beginning of the list
+		 * In order to be able to get the highest distance from the 'source' node,
+		 * and draw the nodes that are not connected to the 'source' node at the outer ring
+		 *
+		 * I believe that this can be optimized
+		 */
+		Object.keys(this.distancesFromSource).forEach(el => {
+			if (this.distancesFromSource[el] === Infinity)
+				this.distancesFromSource[el] = -Infinity
+		});
 		let nodes = Object.keys(this.distancesFromSource).sort(
 			(e1, e2) => {
 				return this.distancesFromSource[e1] - this.distancesFromSource[e2];
 			}
 		);
+
+		const highestSourceDistance = this.distancesFromSource[nodes[nodes.length - 1]];
+
 		nodes.forEach(el => {
+			// Treat vertices that are not connected to the source node
+			if (this.distancesFromSource[el] === -Infinity) this.distancesFromSource[el] = highestSourceDistance + 1;
 			if (numberOfNodesAtRing.hasOwnProperty(this.distancesFromSource[el])) {
 				numberOfNodesAtRing[this.distancesFromSource[el]]++;
 			} else {
@@ -51,12 +68,13 @@ class StarTopology extends Topology {
 			const node = this.nodeHolder[machineKey];
 			const nodeFather = this.nodeHolder[this.fathers[machineKey]];
 			let nodeFatherOffset = {x: 0, y: 0};
-			if(nodeFather != null)
+			if (nodeFather != null)
 				nodeFatherOffset = {x: nodeFather.x, y: nodeFather.y};
 
-			node.x = /*nodeFatherOffset.x + */this.radius * this.distancesFromSource[machineKey] * Math.cos((2 * iterNumRing[this.distancesFromSource[machineKey]] * Math.PI) / numberOfNodesAtRing[this.distancesFromSource[machineKey]]);
-			node.y = /*nodeFatherOffset.y + */this.radius * this.distancesFromSource[machineKey] * Math.sin((2 * iterNumRing[this.distancesFromSource[machineKey]] * Math.PI) / numberOfNodesAtRing[this.distancesFromSource[machineKey]]);
-			iterNumRing[this.distancesFromSource[machineKey]]++;
+			const distancesFromSourceElement = this.distancesFromSource[machineKey];
+			node.x = /*nodeFatherOffset.x + */this.radius * distancesFromSourceElement * Math.cos((2 * iterNumRing[distancesFromSourceElement] * Math.PI) / numberOfNodesAtRing[distancesFromSourceElement]);
+			node.y = /*nodeFatherOffset.y + */this.radius * distancesFromSourceElement * Math.sin((2 * iterNumRing[distancesFromSourceElement] * Math.PI) / numberOfNodesAtRing[distancesFromSourceElement]);
+			iterNumRing[distancesFromSourceElement]++;
 		});
 
 		console.log("Done");
