5f943173884084097d24a4380dd4d0d192ac67b1
Turox <adriel@adriel.eu>
2019-08-19 20:53:35 -0300

    Invert dijkstra filter result graph

--- src/parserLib/Graph/Filter/DijkstraFilter.js
+++ src/parserLib/Graph/Filter/DijkstraFilter.js
@@ -2,72 +2,71 @@ import { FibonacciHeap } from "@tyriar/fibonacci-heap";
 import Filter from "./Filter";
 
 class DijkstraFilter extends Filter {
-	constructor(graphHolder, options) {
-		if(!options.hasOwnProperty("source")){
-			throw "Invoked DijkstraFilter without 'source' option";
-		}
+  constructor(graphHolder, options) {
+    if (!options.hasOwnProperty("source")) {
+      throw "Invoked DijkstraFilter without 'source' option";
+    }
 
-		super(graphHolder, options);
-		this.vertices = Object.keys(this.graphHolder.graph);
-		this.distancesFromSource = {};
-		this.fathers = {};
-	}
+    super(graphHolder, options);
+    this.vertices = Object.keys(this.graphHolder.graph);
+    this.distancesFromSource = {};
+    this.fathers = {};
+  }
 
-	dijkstraShortestPath = function(graph, source) {
-		const dist = {};
-		const prev = {};
-		const heap = new FibonacciHeap();
-		const queue = {};
+  dijkstraShortestPath = function(graph, source) {
+    const dist = {};
+    const prev = {};
+    const heap = new FibonacciHeap();
+    const queue = {};
 
-		const decreaseDist = function(node, newValue) {
-			dist[node] = newValue;
-			heap.decreaseKey(queue[node], newValue);
-		};
+    const decreaseDist = function(node, newValue) {
+      dist[node] = newValue;
+      heap.decreaseKey(queue[node], newValue);
+    };
 
-		this.vertices.forEach(machine => {
-			prev[machine] = null;
-			dist[machine] = Infinity;
-			queue[machine] = heap.insert(Infinity, machine);
-		});
+    this.vertices.forEach(machine => {
+      prev[machine] = null;
+      dist[machine] = Infinity;
+      queue[machine] = heap.insert(Infinity, machine);
+    });
 
-		decreaseDist(source, 0);
+    decreaseDist(source, 0);
 
-		while (!heap.isEmpty()) {
-			const currNode = heap.extractMinimum();
-			const currNodeKey = currNode.value;
-			const neighbors = Object.keys(graph[currNodeKey]);
-			neighbors.forEach(destMachine => {
-				if (!graph[currNodeKey][destMachine]) return;
+    while (!heap.isEmpty()) {
+      const currNode = heap.extractMinimum();
+      const currNodeKey = currNode.value;
+      const neighbors = Object.keys(graph[currNodeKey]);
+      neighbors.forEach(destMachine => {
+        if (!graph[currNodeKey][destMachine]) return;
 
-				const newDist = dist[currNodeKey] + 1;
-				if (newDist < dist[destMachine]) {
-					// Relax
-					decreaseDist(destMachine, newDist);
-					prev[destMachine] = currNodeKey;
-				}
-			});
-		}
-		console.log("Done");
+        const newDist = dist[currNodeKey] + 1;
+        if (newDist < dist[destMachine]) {
+          // Relax
+          decreaseDist(destMachine, newDist);
+          prev[destMachine] = currNodeKey;
+        }
+      });
+    }
+    console.log("Done");
 
-		this.distancesFromSource = dist;
-		this.fathers = prev;
-		return prev;
-	};
+    this.distancesFromSource = dist;
+    this.fathers = prev;
+    return prev;
+  };
 
-	applyFilter() {
-		const { graph } = this.graphHolder;
-		const fathers = this.dijkstraShortestPath(graph, this.options.source);
-		this.vertices.forEach(node => {
-			const neighbors = Object.keys(graph[node]);
-			neighbors.forEach(neighbor => {
-				graph[node][neighbor] = false;
-			});
-			if(fathers[node] != null)
-				graph[node][fathers[node]] = true;
-		});
+  applyFilter() {
+    const { graph } = this.graphHolder;
+    const fathers = this.dijkstraShortestPath(graph, this.options.source);
+    this.vertices.forEach(node => {
+      const neighbors = Object.keys(graph[node]);
+      neighbors.forEach(neighbor => {
+        graph[node][neighbor] = false;
+      });
+      if (fathers[node] != null) graph[fathers[node]][node] = true;
+    });
 
-		console.log("Done");
-	}
+    console.log("Done");
+  }
 }
 
 export default DijkstraFilter;
