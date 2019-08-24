fd261b21ddcc3b53492a6a7b9df77db83de541b0
Turox <adriel@adriel.eu>
2019-08-24 14:14:39 -0300

    fixes on spring algorithm

--- src/parserLib/Graph/Visualizer/Topology/SpringTopology.js
+++ src/parserLib/Graph/Visualizer/Topology/SpringTopology.js
@@ -5,14 +5,14 @@ class SpringTopology extends Topology {
     super(graphHolder, machines, options);
     this.options = options || {};
     this.c1 = this.options.c1 || 2;
-    this.c2 = this.options.c2 || 20;
+    this.c2 = this.options.c2 || 10;
     this.c3 = this.options.c3 || 1;
     this.c4 = this.options.c4 || 0.1;
     this.iterNum = this.options.iterNum || 100;
   }
 
   calculateForcesOnNode(machineKey) {
-    let resultantForce = {x: 0, y:0};
+    let resultantForce = {x: 0, y: 0};
     const edges = this.graphHolder.getEdges(machineKey);
 
 
@@ -21,11 +21,24 @@ class SpringTopology extends Topology {
     });
 
     adjacentVertices.forEach(adjacentMachine => {
-      const xDist = Math.abs(this.nodeHolder[machineKey].x - this.nodeHolder[adjacentMachine].x);
-      const yDist = Math.abs(this.nodeHolder[machineKey].y - this.nodeHolder[adjacentMachine].y);
+      const d = Math.sqrt(Math.pow(this.nodeHolder[machineKey].x - this.nodeHolder[adjacentMachine].x, 2) + Math.pow(this.nodeHolder[machineKey].y - this.nodeHolder[adjacentMachine].y, 2));
+      if(d === 0)
+        return;
 
-      resultantForce.x -= xDist !== 0 ? this.c1 * Math.log(Math.abs(xDist) / this.c2) : 0;
-      resultantForce.y -= yDist !== 0 ? this.c1 * Math.log(Math.abs(yDist) / this.c2) : 0;
+      const force = this.c1 * Math.log(d / this.c2);
+
+      // get the direction where the force is applied
+      const dir = {
+        x: (this.nodeHolder[adjacentMachine].x - this.nodeHolder[machineKey].x) / d,
+        y: (this.nodeHolder[adjacentMachine].y - this.nodeHolder[machineKey].y) / d
+      };
+
+      // apply the force in the normalized vector
+      dir.x *= force;
+      dir.y *= force;
+
+      resultantForce.x += dir.x;
+      resultantForce.y += dir.y;
     });
 
     // Forces that repel
@@ -33,11 +46,24 @@ class SpringTopology extends Topology {
       return !edges[el];
     });
     nonAdjacentVertices.forEach(nonAdjacentMachine => {
-      const xDist = Math.abs(this.nodeHolder[machineKey].x - this.nodeHolder[nonAdjacentMachine].x);
-      const yDist = Math.abs(this.nodeHolder[machineKey].y - this.nodeHolder[nonAdjacentMachine].y);
+      const d = Math.sqrt(Math.pow(this.nodeHolder[machineKey].x - this.nodeHolder[nonAdjacentMachine].x, 2) + Math.pow(this.nodeHolder[machineKey].y - this.nodeHolder[nonAdjacentMachine].y, 2));
+      if(d === 0)
+        return;
 
-      resultantForce.x += xDist !== 0 ? this.c3 / Math.pow(xDist, 2) : 0;
-      resultantForce.y += yDist !== 0 ? this.c3 / Math.pow(yDist, 2) : 0;
+      const force = this.c3 / Math.pow(d, 2);
+
+      // get the direction where the force is applied
+      const dir = {
+        x: (this.nodeHolder[machineKey].x - this.nodeHolder[nonAdjacentMachine].x) / d,
+        y: (this.nodeHolder[machineKey].y - this.nodeHolder[nonAdjacentMachine].y) / d
+      };
+
+      // apply the force in the normalized vector
+      dir.x *= force;
+      dir.y *= force;
+
+      resultantForce.x += dir.x;
+      resultantForce.y += dir.y;
     });
 
     let oldX = this.nodeHolder[machineKey].x;
@@ -46,14 +72,14 @@ class SpringTopology extends Topology {
     resultantForce.x *= this.c4;
     resultantForce.y *= this.c4;
 
-    this.nodeHolder[machineKey].x += resultantForce.x;
-    this.nodeHolder[machineKey].y += resultantForce.y;
+    this.nodeHolder[machineKey].ResX = this.nodeHolder[machineKey].x + resultantForce.x;
+    this.nodeHolder[machineKey].ResY = this.nodeHolder[machineKey].y + resultantForce.y;
 
     let newX = this.nodeHolder[machineKey].x;
     let newY = this.nodeHolder[machineKey].y;
 
 
-    console.log("SprintTopology: " + machineKey + ": OLD {x: " + oldX + ", y: " + oldY + "}, NEW {x: " + newX + ", y: " + newY + "}");
+    console.log("SpringTopology: " + machineKey + ": OLD {x: " + oldX + ", y: " + oldY + "}, NEW {x: " + newX + ", y: " + newY + "}");
   }
 
   updatePositions() {
@@ -62,12 +88,16 @@ class SpringTopology extends Topology {
 
     // Put nodes at random positions, initially
     Object.keys(this.nodeHolder).forEach(machineKey => {
-      this.nodeHolder[machineKey].x = Math.floor(Math.random() * 300);
-      this.nodeHolder[machineKey].y = Math.floor(Math.random() * 300);
+      this.nodeHolder[machineKey].x = Math.floor(Math.random() * 1000);
+      this.nodeHolder[machineKey].y = Math.floor(Math.random() * 1000);
     });
     while (i++ < this.iterNum) {
       Object.keys(this.nodeHolder).forEach(machineKey => {
         this.calculateForcesOnNode(machineKey);
+      });
+      Object.keys(this.nodeHolder).forEach(machineKey => {
+        this.nodeHolder[machineKey].x = this.nodeHolder[machineKey].ResX;
+        this.nodeHolder[machineKey].y = this.nodeHolder[machineKey].ResY;
       });
     }
   }
