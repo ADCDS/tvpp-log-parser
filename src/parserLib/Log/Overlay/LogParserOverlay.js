6cb55717fcedc0fa9e72630f80b235b12957f1a2
Turox <adriel@adriel.eu>
2020-03-10 09:49:31 -0300

    Not considering in partners anymore

--- src/parserLib/Log/Overlay/LogParserOverlay.js
+++ src/parserLib/Log/Overlay/LogParserOverlay.js
@@ -13,8 +13,8 @@ class LogParserOverlay {
 	static lineToEntry(line: string): LogEntryOverlay {
 		/**
 		 *  0.0.0.0:0 works as an separator,
-		 *  to the right are the servers which our 'hostAddress' sends chunks to,
 		 *  to the left are the servers which 'hostAddress' receives packets from.
+		 *  to the right are the servers which our 'hostAddress' sends chunks to,
 		 */
 
 		const pieces = line.split("0.0.0.0:0");
@@ -24,13 +24,15 @@ class LogParserOverlay {
 		const timestamp = firstHalf[1];
 		firstHalf.splice(0, 2);
 
-		const partnersIn = firstHalf
+		// We are not going to consider IN partners
+		const partnersIn = [];
+		/* const partnersIn = firstHalf
 			.filter(value => {
 				return !(value === "" || value === "\r");
 			})
 			.map(str => {
 				return str.trim();
-			});
+			}); */
 
 		const partnersOut = secondHalf
 			.filter(value => {
